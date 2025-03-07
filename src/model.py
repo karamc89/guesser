08a6ff333b9667eb6e3397db635f30890ffa49ea
Daniel Nam <148014770+danielnam11@users.noreply.github.com>
2025-03-06 23:41:30 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,3 +1,45 @@
-# testing
+'''
+RNN concatenates all tokenized and embedded word vectors into a single higher level embedding
+to later pass through a fully connected network
 
-print("hello world")
+Fully connected network also defined in the modelRNN class
+
+Training function defined as well
+'''
+import torch
+import torch.nn as nn
+
+class modelRNN(nn.Module):
+    def __init__(self,input_size, hidden_size, num_class):
+        #input_size = size of input word2vec embeddings, each vector represents one word
+        #hidden_size = size of hidden state vectors
+        #num_class = number of possible job categories to classify to
+        super(modelRNN, self).__init__()
+        self.emb = nn.Embedding.from_pretrained(word2vec.vectors) # word2vec.vectors property to represent a singular resume sample as an array of size (word_count, embedding_size)
+        self.hidden_size = hidden_size
+        self.rnn = nn.RNN(input_size, hidden_size, batch_first = True)
+        self.fc = nn.Linear(hidden_size, num_class)
+
+        def forward(self, x):
+            x = self.emb(x)
+            h0 = torch.zeros(1,x.size(0), self.hidden_size)
+            out,__ = self.rnn(x, h0)
+            return self.fc(out[:,-1,:])
+
+
+def train(model, train, val, n_epochs = 5, lr=1e-5):
+    criterion = nn.CrossEntropyLoss() # can change loss criteria, but CE is most used for classification tasks
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr) #can change optimizer and lr for tuning
+
+    for epoch in range(n_epochs):
+        for resume, label in train: #training data will contain resume content paired with predefined job category label
+            optimizer.zero_grad()
+            prediction = model(resume)
+            loss = criterion(prediction, label)
+            loss.backward()
+            optimizer.step()
+
+
+
+
+
